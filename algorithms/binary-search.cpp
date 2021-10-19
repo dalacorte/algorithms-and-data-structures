80d88da996aaf275e48c515f6ed4b433de540d23
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-19 10:51:46 -0300

    binary search

--- algorithms/binary-search.cpp
+++ algorithms/binary-search.cpp
@@ -3,30 +3,33 @@ using namespace std;
 
 int binarySearch(int arr[], int l, int r, int x)
 {
-    while (l <= r) {
+    while (l <= r)
+    {
         int m = l + (r - l) / 2;
- 
+
         if (arr[m] == x)
             return m;
- 
+
         if (arr[m] < x)
             l = m + 1;
- 
+
         else
             r = m - 1;
     }
- 
+
     return -1;
 }
- 
+
 int main(void)
 {
-    int arr[] = { 1, 2, 3, 4, 5 };
-    int x = 3;
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
     int result = binarySearch(arr, 0, n - 1, x);
-    (result == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at index " << result;
+
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
 
     return 0;
 }
