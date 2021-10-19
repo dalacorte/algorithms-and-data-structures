e6f4bc59778702ebb4fec9cce731b34b3d85338e
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-19 10:50:43 -0300

    linear search

--- algorithms/linear-search.c
+++ algorithms/linear-search.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- 
+
 int search(int arr[], int n, int x)
 {
     int i;
@@ -9,15 +9,15 @@ int search(int arr[], int n, int x)
 
     return -1;
 }
- 
+
 // Driver code
 int main(void)
 {
-    int arr[] = { 1, 2, 3, 4, 5 };
-    int x = 3;
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
-   
     int result = search(arr, n, x);
+
     (result == -1)
         ? printf("Element is not present in array")
         : printf("Element is present at index %d", result);
