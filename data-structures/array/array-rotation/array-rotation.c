e077edf52a1b3024aa56695daef4f3130397aab3
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-20 19:04:17 -0300

    array rotation

--- data-structures/array/array-rotation/array-rotation.c
+++ data-structures/array/array-rotation/array-rotation.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 void printArray(int arr[], int size);
- 
+
 int gcd(int a, int b);
- 
+
 void leftRotate(int arr[], int d, int n)
 {
     int i, j, k, temp;
     d = d % n;
     int g_c_d = gcd(d, n);
-    for (i = 0; i < g_c_d; i++) {
+    for (i = 0; i < g_c_d; i++)
+    {
         temp = arr[i];
         j = i;
-        while (1) {
+        while (1)
+        {
             k = j + d;
             if (k >= n)
                 k = k - n;
@@ -24,14 +26,14 @@ void leftRotate(int arr[], int d, int n)
         arr[j] = temp;
     }
 }
- 
+
 void printArray(int arr[], int n)
 {
     int i;
     for (i = 0; i < n; i++)
         printf("%d ", arr[i]);
 }
- 
+
 int gcd(int a, int b)
 {
     if (b == 0)
@@ -39,10 +41,10 @@ int gcd(int a, int b)
     else
         return gcd(b, a % b);
 }
- 
+
 int main()
 {
-    int arr[] = { 1, 2, 3, 4, 5 };
+    int arr[] = {1, 2, 3, 4, 5};
     leftRotate(arr, 2, 5);
     printArray(arr, 5);
     getchar();
