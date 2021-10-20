5bcd8169d67fd8b2e903358d3dd1709369aabdea
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-10-20 19:04:26 -0300

    array rotation by one

--- data-structures/array/array-rotation/array-rotation-by-one.c
+++ data-structures/array/array-rotation/array-rotation-by-one.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void rotate(int arr[], int n) 
+void rotate(int arr[], int n)
 {
     int x = arr[n - 1], i;
     for (i = n - 1; i > 0; i--)
@@ -18,7 +18,7 @@ int main()
         printf("%d ", arr[i]);
 
     rotate(arr, n);
-    
+
     printf("\nRotated array is\n");
     for (i = 0; i < n; i++)
         printf("%d ", arr[i]);
