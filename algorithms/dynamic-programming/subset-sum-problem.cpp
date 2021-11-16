2d0df473a2306c2d6648b244511a87419899014d
Victor Dalacôrte <victor.dalacorte@talkcomm.com.br>
2021-11-15 23:36:42 -0300

    subset sum problem

--- algorithms/dynamic-programming/subset-sum-problem.cpp
+++ algorithms/dynamic-programming/subset-sum-problem.cpp
@@ -18,9 +18,11 @@ int main()
     int set[] = {3, 34, 4, 12, 5, 2};
     int sum = 9;
     int n = sizeof(set) / sizeof(set[0]);
+
     if (isSubsetSum(set, n, sum) == true)
         cout << "Found a subset with given sum";
     else
         cout << "No subset with given sum";
+
     return 0;
 }
